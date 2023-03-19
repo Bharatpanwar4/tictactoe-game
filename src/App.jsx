0225df289c622d067379cbaf944ab73a9919db9a
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-03-20 03:04:30 +0530

    display draw msg ans style

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Board from './components/Board';
 import './styles.scss';
 import { useState } from 'react';
 import { calculateWinner } from './winner';
+import StatusMessage from './components/StatusMessage';
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(false);
 
 
   const winner = calculateWinner(squares);
-  const nextPlayer = isXNext ? 'X' : "O";
-  const statusMessage = winner ? `Winner is ${winner}` : `Next player is ${nextPlayer}`;
+  
   console.log(winner);
   const handlSquareClick = clickedPosition => {
     if (squares[clickedPosition] || winner) {
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="app">
 
-      <h1> {statusMessage}</h1>
+      <StatusMessage winner={winner} isXNext={isXNext} squares={squares}/>
       <Board squares={squares} handlSquareClick={handlSquareClick} />
 
     </div>
---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,36 @@
+const StatusMessage = ({ winner, isXNext, squares }) => {
+  const noMoveLeft = squares.every(SquareValue => {
+    return SquareValue !== null;
+  });
+
+  const nextPlayer = isXNext ? 'X' : 'O';
+
+  const renderStatusMessage = () => {
+    if (winner) {
+      return <>Winner is <span className={winner=== "X" ? 'text-green' : 'text-orange'}>
+      {winner}
+    </span></>;
+    }
+    if (!winner && noMoveLeft) {
+      return (
+        <>
+          <span className="text-orange">O</span> and{' '}
+          <span className="text-green">X</span> tied
+        </>
+      );
+    }
+    if (!winner && !noMoveLeft) {
+      return (
+        <>
+          Next player is {" "}
+          <span className={isXNext ? 'text-green' : 'text-orange'}>
+            {nextPlayer}
+          </span>
+        </>
+      );
+    }
+    return null;
+  };
+  return <h2 className="status-message">{renderStatusMessage()}</h2>;
+};
+export default StatusMessage;
